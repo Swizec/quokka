95abcf386283792c4e4cb4f44f2a387a86e679b5
Swizec Teller <swizec@swizec.com>
2013-04-08 02:49:32 +0200

    ok login doesn't actually work

--- Quokka.hs
+++ Quokka.hs
@@ -5,30 +5,35 @@ import Network.Shpider
 
 import Secrets
 
-login750 :: IO (ShpiderCode)
-login750 = runShpider $ do
+--login750 :: ShpiderCode
+login750 = do
     download "https://750words.com/auth"
     forms <- getFormsByAction "/auth/signin"
 
-    (loggedin, _) <- case forms of
+    (loggedin, page) <- case forms of
       theForm : _ -> sendForm $ fillOutForm theForm $ pairs $ do
           "person[mail_address]" =: Secrets.username
           "person[password]" =: Secrets.password
-      _ -> download "https://750words.com" -- no login form means we're logged in already
+      --_ -> download "http://google.com" -- no login form means we're logged in already
 
-    return loggedin
+    return page
 
-fetch :: Int -> Int -> IO (Maybe String)
-fetch year month = runShpider $ do
-  (status, page) <- download "http://750words.com/export/2013/4"
+--fetch :: Int -> Int -> Maybe String
+fetch year month = do
+--  (status, page) <- download "http://750words.com/export/2013/4"
+  (status, page) <- download "https://750words.com/export/2013/4"
+
 
   case status of
     Ok -> return $ Just $ source page
     _ -> return Nothing
 
 main = do
-  loggedin <- login750
-  content <- fetch 2013 4
+  content <- runShpider $ do
+    loggedin <- login750
+    --content <- fetch 2013 4
+
+    return loggedin
 
   print content
 --  case loggedin of

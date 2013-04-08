ff1330020c0b366b5d161e985cc03a2a0b022a0b
Swizec Teller <swizec@swizec.com>
2013-04-08 02:09:47 +0200

    a bit more robust login

--- Quokka.hs
+++ Quokka.hs
@@ -10,15 +10,18 @@ import Secrets
 login750 :: IO (Maybe String)
 login750 = runShpider $ do
     download "https://750words.com/auth"
-    theForm : _ <- getFormsByAction "/auth/signin"
-    page <- sendForm $ fillOutForm theForm $ pairs $ do
-          "person[email_address]" =: Secrets.username
+    forms <- getFormsByAction "/auth/signin"
+
+    page <- case forms of
+      theForm : _ -> sendForm $ fillOutForm theForm $ pairs $ do
+          "person[mail_address]" =: Secrets.username
           "person[password]" =: Secrets.password
+      _ -> download "https://750words.com"
 
     case page of
       (Ok, page) -> return $ Just $ source page
-      (_, page) -> return Nothing
+      (_, _) -> return Nothing
 
 main = do
   page <- login750
-  print $ fromJust page
+  print $ page
